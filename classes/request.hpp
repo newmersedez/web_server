1c63dd86337e3f01ee5c9c5272f21a0e9ca47fff
newmersedez <trishkk2001@gmail.com>
2021-10-27 03:15:56 +0300

    template

--- classes/request.hpp
+++ classes/request.hpp
@@ -1,6 +1,25 @@
 #pragma once
 
+#include <string>
+#include <cstdlib>
+
 class HTTPRequest
 {
+private:
+	std::string	_method;
+	std::string	_path;
+	std::string	_version;
+	std::string	_connectStatus;
+	std::string	_connectType;
+	size_t		_connectLength;
 
+public:
+	HTTPRequest()
+		: _method(""), _path(""), _version(""),
+		_connectStatus(""), _connectType(""), _connectLength(0)
+	{}
+
+	
+
+	~HTTPRequest() = default;
 };
